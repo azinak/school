abc6d654eec711620957f1e624fda07fecd958d6
Abdelaziz Bouchaba <abdoupyblender@gmail.com>
2020-10-23 09:58:45 +0100

    aziz test test

--- stuff/views.py
+++ stuff/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 from sahh.models import Courses, CustomUser, Department, Enterprise, Nursery, Sections, Staffs, Students, Teacher
 
 def show_demo_page(request):
-    return render(request, 'index.html')
+    return render(request, 'stuff_template/index.html')
